ab12b28f96cfede28afb53dde396623ca8c9ae14
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-02-16 19:27:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,22 @@
 import streamlit as st
 from datamanager import SupaBaseDataManager
 
-st.title("Product matcher")
+#st.title("Product matcher")
 
 dm = SupaBaseDataManager()
 
-st.write("Hello Streamlit 🚀")
+#st.write("Hello Streamlit 🚀")
+
+st.title("Ár összehasonlító")
+
+keyword = st.text_input("Keresőszó")
+
+if keyword:
+    own_df, raw_df = dm.get_products_by_keyword(keyword)
+
+    st.subheader("Saját termékek")
+    st.dataframe(own_df)
+
+    st.subheader("Beszállítók")
+    st.dataframe(raw_df)
+
