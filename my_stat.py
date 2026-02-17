1fd940aa7e1b394956ceb199cc245b01bdd9a2c6
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-02-17 21:09:07 +0100

    u

--- my_stat.py
+++ my_stat.py
@@ -1,15 +1,16 @@
 from datamanager import SupaBaseDataManager
 
 class Statistic:
+    dm = SupaBaseDataManager()
 
-    def count_shops(self, filters=None):
-        return self.count_rows("webshops", filters)
+    def count_shops(filters=None):
+        return dm.count_rows("webshops", filters)
 
-    def count_clusters(self, filters=None):
-        return self.count_rows("clusters", filters)
+    def count_clusters(filters=None):
+        return dm.count_rows("clusters", filters)
 
-    def count_own_products(self, filters=None):
-        return self.count_rows("own_product", filters)
+    def count_own_products(filters=None):
+        return dm.count_rows("own_product", filters)
 
-    def count_raw_products(self, filters=None):
-        return self.count_rows("raw_product", filters)
+    def count_raw_products(filters=None):
+        return dm.count_rows("raw_product", filters)
