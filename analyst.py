2294fcd55d02cb7c2de607c966fe6feef65a5000
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-02-17 16:03:32 +0100

    Update analyst.py

--- analyst.py
+++ analyst.py
@@ -10,6 +10,8 @@ class PriceAnalyst:
         clusters = self.dm.get_clusters(keyword)
         own, raw = self.dm.get_products_by_keyword(keyword)
         links = self.dm.read_data("product_cluster_links")
+        print("links type:", type(links))
+        print("links:", links)
         shops = self.dm.read_data("webshops")
 
         df = pd.concat([
