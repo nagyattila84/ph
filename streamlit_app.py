2bba26cb405c432e192e53aa9508b43713ef0539
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-02-19 15:05:33 +0100

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -256,7 +256,7 @@ if not st.session_state.logged_in:
     login_page()
 
 else:
-    st.image("PriceHunter-logo-négyzet.png", caption="PriceHunter logó", use_container_width=True)
+    st.sidebar.image("PriceHunter-logo-négyzet.png", caption="PriceHunter logó", use_container_width=True)
     st.sidebar.write(f"Bejelentkezve: {st.session_state.username}")
 
     if st.sidebar.button("Kijelentkezés"):
