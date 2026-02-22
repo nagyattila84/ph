65e7385f6bdd09a515c3f087d447004900cc1c18
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-02-22 14:37:58 +0100

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -198,12 +198,12 @@ def page_controlpanel():
     st.sidebar.header("📊 Vezérlőpult")
     #st.sidebar.radio("Menü", ["Saját árak feltöltése", "Konkures árak lekérdezése", "Termék párosítás", "Árak összehasonlítása"])
   
-    #st.set_page_config(layout="wide")
+    st.set_page_config(layout="wide")
     st.title("📊 Vezérlőpult")
     
     st.header("0. Saját árak feltöltése")
     st.write("Az own_product táblába manuálisan kerülnek a saját termék adatok.")
-    
+
     st.header("1. Árak letöltése")
     st.write("Termék árak letöltése konkurens oldalakról.")
     st.write("Webáruházak kijelölése:")
