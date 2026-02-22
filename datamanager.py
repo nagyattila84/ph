ee42680e6c20606dabb7517475c735dabe19c81e
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-02-22 21:14:24 +0100

    u

--- datamanager.py
+++ datamanager.py
@@ -1,21 +1,22 @@
 import os
 import pandas as pd
 from supabase import create_client, Client
-
+from scraper import Scraper
 import streamlit as st
 
 
 # @title
 class SupaBaseDataManager:
   def __init__(self):
-      SUPABASE_URL=st.secrets["supabase"]["url"]
-      SUPABASE_KEY=st.secrets["supabase"]["key"]
-      try:
-          self.client = create_client(SUPABASE_URL, SUPABASE_KEY)
-          print("Successfully connected to Supabase.")
-      except Exception as e:
-          print(f"Error connecting to Supabase: {e}")
-          self.client = None
+    SUPABASE_URL=st.secrets["supabase"]["url"]
+    SUPABASE_KEY=st.secrets["supabase"]["key"]
+    try:
+        self.client = create_client(SUPABASE_URL, SUPABASE_KEY)
+        print("Successfully connected to Supabase.")
+    except Exception as e:
+        print(f"Error connecting to Supabase: {e}")
+        self.client = None
+    sc = Scraper()
 
   def insert_products_from_df(self, df: pd.DataFrame):
           #Upload scraped products from DataFrame into Supabase.
