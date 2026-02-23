0e15e1c0915d0c82f1058b941a5d04569e88fb86
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-02-23 16:03:20 +0100

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -231,7 +231,10 @@ def page_controlpanel():
     if st.button("Árak letöltése", type="primary"):
         selected_shops = shops[shops["selected"]]
         price = sc.get_price_from_multi_webshop_df(selected_shops, keyword)
-        
+        st.set_page_config(
+            page_title="Ex-stream-ly Cool App",
+            page_icon="🧊",
+            layout="wide")
         st.table(price)
 
     st.header("2.Termék párosítás")
