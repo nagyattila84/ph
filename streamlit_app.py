a5933cdd345011e2f6d39057c4c298a7475abcf0
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-02-23 16:12:03 +0100

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -230,9 +230,8 @@ def page_controlpanel():
 
     if st.button("Árak letöltése", type="primary"):
         selected_shops = shops[shops["selected"]]
-        with st.status("Webshopok feldolgozása...", expanded=True) as status:
+        with st.status("Árak letöltése folyamatban...", expanded=True) as status:
             price = sc.get_price_from_multi_webshop_df(selected_shops, keyword)
-            status.update(label="Kész!", state="complete")
         st.set_page_config(layout="wide")
         st.table(price)
 
