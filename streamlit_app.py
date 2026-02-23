8243a60f3a5e64f3064957f5fad80dbecba305e3
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-02-23 15:01:21 +0100

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,6 @@ def login_page():
 
 # ================= PAGES =================
 def page_dashboard():
-    st.write(st.secrets.supabase.url)
-    st.write(st.secrets.supabase.key)
     db = SupaBaseDataManager(st.secrets.supabase.url, st.secrets.supabase.key)
     stat = Statistic(st.secrets.supabase.url, st.secrets.supabase.key)
 
