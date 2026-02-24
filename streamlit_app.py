72c56b67b789aed9354dc8d19e3ede7adc3d783e
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-02-24 15:53:41 +0100

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -238,6 +238,7 @@ def page_controlpanel():
 
             with st.spinner("Árak letöltése folyamatban..."):
                 price = sc.get_price_from_multi_webshop_df(selected_shops, keyword)
+            st.session_state.scraped_prices = price
             st.success("Kész!")
             
             expander = st.expander("Árak megtekintése")
