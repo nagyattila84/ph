de5f6e0e2d3b1340f450d69f7fa01cdc614ff7b2
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-02-24 16:58:16 +0100

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,9 @@ def page_dashboard():
     fig = px.pie(df, names="type", values="count", hole=0.4)
     st.plotly_chart(fig, use_container_width=True)
 
+    st.table(dm.get_view("webshop_product_stats")
     st.bar_chart(dm.get_view("webshop_product_stats"))
+    #x="year", y="yield", color="site", stack=False
 
 def page_search():
     dm = SupaBaseDataManager(st.secrets.supabase.url, st.secrets.supabase.key)
