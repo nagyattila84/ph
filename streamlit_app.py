eb9c288d79f6515b68dba0209d1e95ff7dd61094
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-02-26 21:48:52 +0100

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -288,6 +288,8 @@ def page_controlpanel():
     clusters = dm.read_data("clusters")
     products_without_cluster = dm.get_unclustered_products_by_webshops(selected_ids)
     
+    st.table(products_without_cluster)
+
     st.write("products type:", type(products_without_cluster))
     st.write("clusters type:", type(clusters))
     if st.button("🔗 Termékek párosítása", type="primary"):
