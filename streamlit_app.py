a1d8f577d552ad352dafc80b1acb1eb087b12ce5
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-02-27 21:23:04 +0100

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -263,46 +263,46 @@ def page_controlpanel():
     #*************
     st.header("2.Termék párosítás")
     if st.button("Saját termékek betöltése"):
-        expander2 = st.expander("Saját termékek")
-        own_products_without_cluster = dm.read_data(
-            table_name = "own_products",
-            is_null = ["cluster_id"]
-        )
-        expander2.table(own_products_without_cluster)
+    st.session_state["own_products_without_cluster"] = dm.read_data(
+        table_name="own_products",
+        is_null=["cluster_id"]
+    )
+    expander2 = st.expander("Saját termékek")
+    expander2.table(own_products_without_cluster)
 
-        if st.button("Saját termékek clusterezése"):
-            clusters = dm.read_data("clusters")
-            with st.spinner("Párosítás folyamatban..."):            
+    if st.button("Saját termékek clusterezése"):
+        clusters = dm.read_data("clusters")
+        with st.spinner("Párosítás folyamatban..."):            
 
-                matched_df = pm.match_products(own_products_without_cluster, clusters)
-                st.header("SIKERES PÁROSÍTÁS")
+            matched_df = pm.match_products(own_products_without_cluster, clusters)
+            st.header("SIKERES PÁROSÍTÁS")
 
-                total = len(matched_df)
-                matched_count = len(matched_df[matched_df["is_new_cluster"] == False])
-                new_cluster_count = len(matched_df[matched_df["is_new_cluster"] == True])
+            total = len(matched_df)
+            matched_count = len(matched_df[matched_df["is_new_cluster"] == False])
+            new_cluster_count = len(matched_df[matched_df["is_new_cluster"] == True])
 
-                st.write("💎 Szép dashboard megjelenítés")
-                col1, col2, col3 = st.columns(3)
+            st.write("💎 Szép dashboard megjelenítés")
+            col1, col2, col3 = st.columns(3)
 
-                col1.metric("📦 Feldolgozott termék", total)
-                col2.metric("🔗 Clusterhez kapcsolva", matched_count)
-                col3.metric("🆕 Új cluster szükséges", new_cluster_count)
+            col1.metric("📦 Feldolgozott termék", total)
+            col2.metric("🔗 Clusterhez kapcsolva", matched_count)
+            col3.metric("🆕 Új cluster szükséges", new_cluster_count)
 
-                st.write("🚀 Extra: százalékos arány")
-                if total > 0:
-                    match_ratio = round(matched_count / total * 100, 1)
-                    st.info(f"Match arány: {match_ratio}%")
-                
-                st.write("🎯 Ha külön akarod listázni")
-                with st.expander("🔗 Kapcsolt termékek"):
-                    st.dataframe(
-                        matched_df[matched_df["is_new_cluster"] == False]
-                    )
+            st.write("🚀 Extra: százalékos arány")
+            if total > 0:
+                match_ratio = round(matched_count / total * 100, 1)
+                st.info(f"Match arány: {match_ratio}%")
+            
+            st.write("🎯 Ha külön akarod listázni")
+            with st.expander("🔗 Kapcsolt termékek"):
+                st.dataframe(
+                    matched_df[matched_df["is_new_cluster"] == False]
+                )
 
-                with st.expander("🆕 Új clusterre váró termékek"):
-                    st.dataframe(
-                        matched_df[matched_df["is_new_cluster"] == True]
-                    )
+            with st.expander("🆕 Új clusterre váró termékek"):
+                st.dataframe(
+                    matched_df[matched_df["is_new_cluster"] == True]
+                )
             
     
     threshold = st.slider("Minimum score?", 60, 100, 80)
