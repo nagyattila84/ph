317585102cb75bf539a1826ac42253d5b123e2cc
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-02-28 19:27:33 +0100

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ if "username" not in st.session_state:
 
 # ================= LOGIN =================
 def login_page():   
-    st.sidebar.image("PriceHunter-logo-fekvő.png", width=400, use_container_width=True)
+    st.logo("PriceHunter-logo-fekvő.png", width=400, use_container_width=True)
     st.title("Bejelentkezés")
     username = st.text_input("Felhasználónév")
     password = st.text_input("Jelszó", type="password")
