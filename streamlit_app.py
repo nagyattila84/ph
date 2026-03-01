dd150ff0354a58547529358627f8937a294ee907
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-03-01 12:17:35 +0100

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ if "username" not in st.session_state:
 def login_page():   
     
     c1, c2, c3 = st.columns(3)
-    c2.image("Images\PriceHunter-logo-fekvő.png", width=300, use_container_width=False)
+    c2.image("Images/PriceHunter-logo-fekvő.png", width=300, use_container_width=False)
 
     c2.title("Bejelentkezés")
     username = c2.text_input("Felhasználónév", width=300)
@@ -637,7 +637,7 @@ if not st.session_state.logged_in:
     login_page()
 
 else:  
-    st.sidebar.image("Images\PriceHunter-logo-fekvő.png", width=400, use_container_width=True)
+    st.sidebar.image("Images/PriceHunter-logo-fekvő.png", width=400, use_container_width=True)
     st.sidebar.write(f"Bejelentkezve: {st.session_state.username}")
 
     if st.sidebar.button("Kijelentkezés"):
