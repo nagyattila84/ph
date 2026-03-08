1782ae9a6dbcd2069817632563c3c9c34c665e5f
Attila Nagy <35396564+nagyattila84@users.noreply.github.com>
2026-03-08 20:53:57 +0100

    u

--- matcher.py
+++ matcher.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 from rapidfuzz import fuzz
 from difflib import SequenceMatcher
-from datetime import datetime
+from datetime import date
 
 #csoportosítás optimalizált!
 class ProductMatcher:
